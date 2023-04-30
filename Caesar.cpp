70534fbcb2270b93c475e5f28d297410caebc735
rower <44478781+rower02@users.noreply.github.com>
2023-04-30 22:51:13 +0200

    Initial commit

---
+++ Caesar.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void szyfruj(string tekst, int klucz) {
+	string zaszyfrowany;
+	for (int i = 0; i < tekst.length(); i++) {
+		if (tekst[i] != '\0') {
+			if (isupper(tekst[i]))
+			{
+				if (tekst[i] + klucz > 90) {
+					char znak = tekst[i] + klucz - 26;
+					zaszyfrowany += znak;
+				}
+				else
+				{
+					char znak = tekst[i] + klucz;
+					zaszyfrowany += znak;
+				}
+			}
+			else {
+				if (tekst[i] + klucz > 122) {
+					char znak = tekst[i] + klucz - 26;
+					zaszyfrowany += znak;
+				}
+				else
+				{
+					char znak = tekst[i] + klucz;
+					zaszyfrowany += znak;
+				}
+			}
+		}
+	}
+	cout << zaszyfrowany << endl;
+}
+
+void odszyfruj(string tekst, int klucz) {
+	string odszyfrowany;
+	for (int i = 0; i < tekst.length(); i++) {
+		if (tekst[i] != '\0') {
+			if (isupper(tekst[i]))
+			{
+				if (tekst[i] - klucz < 65) {
+					char znak = tekst[i] - klucz + 26;
+					odszyfrowany += znak;
+				}
+				else
+				{
+					char znak = tekst[i] - klucz;
+					odszyfrowany += znak;
+				}
+			}
+			else {
+				if (tekst[i] + klucz < 122) {
+					char znak = tekst[i] - klucz + 26;
+					odszyfrowany += znak;
+				}
+				else
+				{
+					char znak = tekst[i] - klucz;
+					odszyfrowany += znak;
+				}
+			}
+		}
+	}
+	cout << odszyfrowany << endl;
+}
+
+int main()
+{
+	string tekst;
+	int klucz;
+	cout << "Podaj tekst do zaszyfrowania: ";
+	cin >> tekst;
+	cout << "Podaj klucz(o ile miejsc ma byc przesuniety tekst): ";
+	cin >> klucz;
+	szyfruj(tekst, klucz);
+	cout << "Podaj tekst do odszyfrowania: ";
+	cin >> tekst;
+	cout << "Podaj klucz(o ile miejsc byl przesuniety tekst): ";
+	cin >> klucz;
+	odszyfruj(tekst, klucz);
+}
